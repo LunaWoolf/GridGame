8d93d9888505457b43f840266297764a5d66e98f
Luna <yw3264@nyu.edu>
2019-03-30 18:59:32 -0400

    3.30

--- GridGame/Assets/BoardManager.cs
+++ GridGame/Assets/BoardManager.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int row = 4;
+    int col = 6;
+    public int[,] Gems_List = new int[4, 6];
+    public Sprite Gem_Image;
+    public GameObject Gems;
+    public GameObject Player;
+
     void Start()
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                Gems_List[i,j] = Random.Range(1, 7);
+                GameObject Gem = Instantiate(Gems);
+                SpriteRenderer SpriteRender = Gem.GetComponent<SpriteRenderer>();
+                switch(Gems_List[i, j])
+                {
+                    case 1:
+                        SpriteRender.color = Color.red;
+                        break;
+                    case 2:
+                        SpriteRender.color = Color.blue;
+                        break;
+                    case 3:
+                        SpriteRender.color = Color.green;
+                        break;
+                    case 4:
+                        SpriteRender.color = Color.yellow;
+                        break;
+                    case 5:
+                        SpriteRender.color = new Color (1,0.6f,0,1);
+                        break;
+                    case 6:
+                        SpriteRender.color = new Color (0.6f,0.3f,0.9f,1);
+                        break;
+                }
+                Gem.transform.position = new Vector3 (i, j-1, 0);
+            }
+
+        }
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void PlacePlayer()
     {
-        
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if(Gems_List[i,j] == 7)
+                {
+                    Player.transform.position = new Vector3(i, j - 1, 0);
+                }
+            }
+
+        }
+
     }
 }
---
+++ GridGame/Assets/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Player;
+    GameObject BoardManager;
+    BoardManager BoardManager_Script;
+
+    void Start()
+    {
+        BoardManager = GameObject.Find("BoardManager");
+        BoardManager_Script = BoardManager.GetComponent<BoardManager>();
+        BoardManager_Script.Gems_List[2,3] = 7;
+        BoardManager.SendMessage("PlacePlayer");
+    }
+
+
+    void Update()
+    {
+
+        
+    }
+}
