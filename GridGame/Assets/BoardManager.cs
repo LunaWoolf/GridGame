f232c9a09fc01e45aec03c2baed73006661f3f3c
Luna <yw3264@nyu.edu>
2019-04-05 01:15:00 -0400

    4.4 (Swap & Vertically detect match & fall system )

--- GridGame/Assets/BoardManager.cs
+++ GridGame/Assets/BoardManager.cs
@@ -4,67 +4,221 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    int row = 4;
-    int col = 6;
-    public int[,] Gems_List = new int[4, 6];
+    int row = 5;
+    int col = 7;
+    public int[,] Gems_List = new int[5, 7];
     public Sprite Gem_Image;
     public GameObject Gems;
-    public GameObject Player;
+    public GameObject Player_Perfab;
+    GameObject Player;
+    int[] Player_Pos_Start = new int[4];
 
     void Start()
     {
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < col; j++)
-            {
-                Gems_List[i,j] = Random.Range(1, 7);
-                GameObject Gem = Instantiate(Gems);
-                SpriteRenderer SpriteRender = Gem.GetComponent<SpriteRenderer>();
-                switch(Gems_List[i, j])
-                {
-                    case 1:
-                        SpriteRender.color = Color.red;
-                        break;
-                    case 2:
-                        SpriteRender.color = Color.blue;
-                        break;
-                    case 3:
-                        SpriteRender.color = Color.green;
-                        break;
-                    case 4:
-                        SpriteRender.color = Color.yellow;
-                        break;
-                    case 5:
-                        SpriteRender.color = new Color (1,0.6f,0,1);
-                        break;
-                    case 6:
-                        SpriteRender.color = new Color (0.6f,0.3f,0.9f,1);
-                        break;
-                }
-                Gem.transform.position = new Vector3 (i, j-1, 0);
-            }
-
-        }
+        Player_Pos_Start[0] = 2;
+        Player_Pos_Start[1] = 3;
+        Player_Pos_Start[2] = 2;
+        Player_Pos_Start[3] = 3;
+        LoadStartBoard();
+        //PlacePlayer(Player_Pos_Start);
     }
 
     void Update()
     {
-        
+
     }
 
-    void PlacePlayer()
+    void LoadStartBoard()
     {
+        Player = Instantiate(Player_Perfab);
+        Player.transform.position = new Vector3(2, 3 - 1, 0);
         for (int i = 0; i < row; i++)
         {
             for (int j = 0; j < col; j++)
             {
-                if(Gems_List[i,j] == 7)
+                Gems_List[i, j] = Random.Range(1, 7);
+                Gems_List[2, 3] = 7;
+                if (Gems_List[i, j] != 7)
                 {
-                    Player.transform.position = new Vector3(i, j - 1, 0);
+                    GameObject Gem = Instantiate(Gems);
+                    SpriteRenderer SpriteRender = Gem.GetComponent<SpriteRenderer>();
+                    switch (Gems_List[i, j])
+                    {
+                        case 1:
+                            SpriteRender.color = Color.red;
+                            break;
+                        case 2:
+                            SpriteRender.color = Color.blue;
+                            break;
+                        case 3:
+                            SpriteRender.color = Color.green;
+                            break;
+                        case 4:
+                            SpriteRender.color = Color.yellow;
+                            break;
+                        case 5:
+                            SpriteRender.color = new Color(1, 0.6f, 0, 1);
+                            break;
+                        case 6:
+                            SpriteRender.color = new Color(0.6f, 0.3f, 0.9f, 1);
+                            break;
+
+                    }
+                    Gem.transform.position = new Vector3(i, j - 1, 0);
+
                 }
+
             }
 
         }
+        CheckMatch(Player_Pos_Start);
 
     }
+
+    void LoadBoard(int[] Player_Pos)
+    {
+        GameObject[] OldGems;
+
+        OldGems = GameObject.FindGameObjectsWithTag("Gem");
+
+        foreach (GameObject Gem in OldGems)
+        {
+            Destroy(Gem);
+        }
+        //-------------------------------------------------------
+        Gems_List[Player_Pos[2], Player_Pos[3]] = 7;
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (Gems_List[i, j] != 7)
+                {
+                    GameObject Gem = Instantiate(Gems);
+                    SpriteRenderer SpriteRender = Gem.GetComponent<SpriteRenderer>();
+                    switch (Gems_List[i, j])
+                    {
+                        case 1:
+                            SpriteRender.color = Color.red;
+                            break;
+                        case 2:
+                            SpriteRender.color = Color.blue;
+                            break;
+                        case 3:
+                            SpriteRender.color = Color.green;
+                            break;
+                        case 4:
+                            SpriteRender.color = Color.yellow;
+                            break;
+                        case 5:
+                            SpriteRender.color = new Color(1, 0.6f, 0, 1);
+                            break;
+                        case 6:
+                            SpriteRender.color = new Color(0.6f, 0.3f, 0.9f, 1);
+                            break;
+                    }
+                    Gem.transform.position = new Vector3(i, j - 1, 0);
+
+                }
+
+            }
+
+        }
+    }
+
+
+    void PlacePlayer(int[] Player_Pos)
+    {
+
+        //Debug.Log("call");
+        int row = Player_Pos[2];
+        int col = Player_Pos[3] - 1;
+        Player.transform.position = new Vector3(row, col, 0);
+
+    }
+
+
+    void SwapGem(int[] Player_Pos)
+    {
+        Gems_List[Player_Pos[0], Player_Pos[1]] = Gems_List[Player_Pos[2], Player_Pos[3]];
+
+    }
+
+
+    void CheckMatch(int[] Player_Pos)
+    {
+        int match = 0;
+        int oldnumber = 0;
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                int newnumber = Gems_List[i, j];
+                if (newnumber == oldnumber)
+                {
+                    match++;
+                }
+                else
+                {
+                    if (match >= 2)
+                    {
+                        Debug.Log("match");
+                        Debug.Log(match);
+                        Gems_List[i, j - 1] = 0;
+                        Gems_List[i, j - 2] = 0;
+                        Gems_List[i, j - 3] = 0;
+                        /*if (match == 3)
+                        {
+                            Gems_List[row, col - 4] = 0;
+                        }
+                        if (match == 4)
+                        {
+                            Gems_List[row, col - 4] = 0;
+                        }*/
+                        match = 0;
+                        GemsFall(Player_Pos);
+                    }
+                    match = 0;
+                }
+                oldnumber = Gems_List[i, j];
+            }
+
+            match = 0;
+            oldnumber = 0;
+        }
+    }
+
+    void GemsFall(int[] Player_Pos)
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col -1; j++)
+            {
+                if (Gems_List[i, j] == 0)
+                {
+                    Gems_List[i, j] = Gems_List[i, j + 1];
+                    Gems_List[i, j + 1] = 0;
+                }
+            }
+        }
+        //——————————————————————————————————————————————————————
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (Gems_List[i, j] == 0)
+                {
+                    Gems_List[i, j] = Random.Range(1, 7);
+                }
+            }
+        }
+
+        LoadBoard(Player_Pos);
+    }
+
+
+
+
+
 }
+
--- GridGame/Assets/GameManager.cs
+++ GridGame/Assets/GameManager.cs
@@ -7,19 +7,68 @@ public class GameManager : MonoBehaviour
     public GameObject Player;
     GameObject BoardManager;
     BoardManager BoardManager_Script;
+    int Player_row = 2;
+    int Player_col = 3;
+    int[] Player_Pos = new int[4];
+    //int[] Swap_Pos = new int[4];
 
     void Start()
     {
         BoardManager = GameObject.Find("BoardManager");
         BoardManager_Script = BoardManager.GetComponent<BoardManager>();
-        BoardManager_Script.Gems_List[2,3] = 7;
-        BoardManager.SendMessage("PlacePlayer");
+       
     }
 
 
     void Update()
     {
+        if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            Player_Pos[0] = Player_row;
+            Player_Pos[1] = Player_col;
+            //Debug.Log("A");
+            Player_row--;
+            MovePlayer();
+        }
 
-        
+        if (Input.GetKeyUp(KeyCode.D)||Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            Player_Pos[0] = Player_row;
+            Player_Pos[1] = Player_col;
+            //Debug.Log("D");
+            Player_row++;
+            MovePlayer();
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.W)|| Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            Player_Pos[0] = Player_row;
+            Player_Pos[1] = Player_col;
+            //Debug.Log("W");
+            Player_col++;
+            MovePlayer();
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.S)|| Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            Player_Pos[0] = Player_row;
+            Player_Pos[1] = Player_col;
+            //Debug.Log("S");
+            Player_col--;
+            MovePlayer();
+
+        }
+    }
+
+    void MovePlayer()
+    {
+        Player_Pos[2] = Player_row;
+        Player_Pos[3] = Player_col;
+        BoardManager.SendMessage("SwapGem", Player_Pos);
+        BoardManager.SendMessage("LoadBoard", Player_Pos);
+        BoardManager.SendMessage("PlacePlayer", Player_Pos);
+        BoardManager.SendMessage("CheckMatch", Player_Pos);
     }
 }
